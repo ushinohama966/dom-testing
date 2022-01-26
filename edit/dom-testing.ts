87bd5896de1ee51f376e56731fde6d4e76c3798d
lkl191 <shigotoyou1985@gmail.com>
2022-01-26 14:45:35 +0900

    add scroll action

--- edit/dom-testing.ts
+++ edit/dom-testing.ts
@@ -16,7 +16,7 @@ export const clickButton = (id: string) => {
       }, 200);
       */
     } else {
-      console.log("id: " + id + "が見つかりませんでした");
+      console.log("not found id: " + id);
       reject("clickButton");
     }
   });
@@ -40,6 +40,7 @@ export const inputString = (id: string, str: string, inputTime?: number) => {
       input.value = "";
       const timer = setInterval(() => {
         input.value += str[len_cnt];
+        // input.innerText += str[len_cnt]
         len_cnt++;
         if (len_cnt >= str.length) {
           clearInterval(timer);
@@ -49,8 +50,18 @@ export const inputString = (id: string, str: string, inputTime?: number) => {
       }, sleep_time);
     } else {
       // reject(console.log(test_name + " >>> failed"));
+      console.log("not found id: " + id);
       reject(0);
     }
+  });
+};
+
+const sleep = (sleep_time: number) => {
+  return new Promise((resolve) => {
+    const timer = setInterval(() => {
+      clearTimeout(timer);
+      resolve(1);
+    }, sleep_time);
   });
 };
 
@@ -58,12 +69,45 @@ export const testSleep = (sleep_time: number) => {
   return new Promise((resolve, reject) => {
     console.log("test is sleeping...");
     try {
-      const timer = setInterval(() => {
-        clearTimeout(timer);
+      sleep(sleep_time).then(() => {
         resolve(1);
-      }, sleep_time);
+      });
     } catch (err) {
       reject(console.error(err));
+    }
+  });
+};
+
+// cnt is not good
+// complexity recursion
+
+export const scroll = (x: number, y: number, cntY = 1, cntX = 1) => {
+  return new Promise((resolve, reject) => {
+    try {
+      const shiftY = y - window.scrollY;
+      const shiftX = x - window.scrollX;
+
+      if (shiftY < 0) cntY = -1;
+      if (shiftX < 0) cntX = -1;
+      const newY = window.scrollY + cntY;
+      const newX = window.scrollX + cntX;
+      if (shiftY == 0 && shiftX == 0) {
+        window.scroll(x, y);
+        return resolve(1);
+      } else if (shiftX == 0) {
+        window.scroll(x, newY);
+      } else if (shiftY == 0) {
+        window.scroll(newX, y);
+      } else {
+        window.scroll(newX, newY);
+      }
+      sleep(1).then(() => {
+        scroll(x, y).then(() => {
+          return resolve(1);
+        });
+      });
+    } catch (err) {
+      reject(console.log(err));
     }
   });
 };
--- lib/dom-testing.d.ts
+++ lib/dom-testing.d.ts
@@ -1,4 +1,5 @@
 export declare const clickButton: (id: string) => Promise<unknown>;
 export declare const inputString: (id: string, str: string, inputTime?: number | undefined) => Promise<unknown>;
 export declare const testSleep: (sleep_time: number) => Promise<unknown>;
+export declare const scroll: (x: number, y: number, cntY?: number, cntX?: number) => Promise<unknown>;
 export declare const syncDoTest: (tests: any[], startIndex?: number) => void;
